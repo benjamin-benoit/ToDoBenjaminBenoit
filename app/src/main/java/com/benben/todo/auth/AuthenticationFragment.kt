ddd03d4e164073ef930346b554e64c5ebf763322
Benjamin Benoit <benjaminbenoitfr@gmail.com>
2020-02-21 14:20:00 +0100

    @

--- app/src/main/java/com/benben/todo/auth/AuthenticationFragment.kt
+++ app/src/main/java/com/benben/todo/auth/AuthenticationFragment.kt
@@ -1,13 +1,13 @@
 package com.benben.todo.auth
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.benben.todo.R
+import kotlinx.android.synthetic.main.fragment_authentication.*
 
 class AuthenticationFragment : Fragment() {
     override fun onCreateView(
@@ -16,5 +16,21 @@ class AuthenticationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_authentication, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttonLogin = buttonLogin
+        val buttonSignIn = buttonSignUp
+        buttonLogin.setOnClickListener{
+            findNavController().navigate(R.id.action_authenticationFragment_to_loginFragment)
+        }
+        buttonSignUp.setOnClickListener {
+            findNavController().navigate(R.id.action_authenticationFragment_to_signupFragment)
+        }
     }
 }
---
+++ app/src/main/java/com/benben/todo/auth/LoginResponse.kt
@@ -0,0 +1,14 @@
+package com.benben.todo.auth
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+import java.io.Serializable
+
+@Parcelize
+data class LoginResponse(
+    @field:Json(name = "email")
+    val id: String,
+    @field:Json(name = "password")
+    val title: String
+): Serializable, Parcelable
